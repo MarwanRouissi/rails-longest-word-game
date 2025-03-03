d274c9fbaa9bc2dd53e26d8d8e5fd0a2a7c4b801
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-03 16:43:05 +0100

    added app working

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def new
+    @letters = 10.times.map { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    # @grid = params[:grid]
+    url = "https://dictionary.lewagon.com/#{params[:word]}"
+    word_serialized = URI.parse(url).read
+    word_valid = JSON.parse(word_serialized)['found']
+    if !word_valid
+      @msg = "Sorry but #{params[:word]} does not seem to be a valid English word..."
+    elsif !word_valid?(params[:word], params[:grid].split)
+      @msg = "Sorry but #{params[:word]} can't be built out of #{params[:grid]}"
+    else
+      @msg = "Congratulations! #{params[:word]} is a valid English word!"
+    end
+  end
+
+  def word_valid?(word, grid)
+    word.upcase.each_char do |char|
+      index = grid.index(char)
+      return false if index.nil?
+
+      grid.delete_at(index)
+    end
+    true
+  end
+end
