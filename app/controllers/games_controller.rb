c7e40a4f1ae0d7ae83e685b64c7d9daf4039947b
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-03 19:25:40 +0100

    added some style to /new view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,14 +8,17 @@ class GamesController < ApplicationController
     url = "https://dictionary.lewagon.com/#{params[:word]}"
     word_serialized = URI.parse(url).read
     word_valid = JSON.parse(word_serialized)['found']
-    if !word_valid
-      @msg = "Sorry but <b>#{params[:word]}</b> does not seem to be a valid English word..."
-    elsif !word_valid?(params[:word], params[:grid].split)
-      @msg = "Sorry but <b>#{params[:word]}</b> can't be built out of #{params[:grid]}"
+    is_valid = word_valid?(params[:word].upcase, params[:grid].upcase.split)
+    if !is_valid
+      @msg = "Sorry but <b>#{params[:word]}</b> can't be built out of #{params[:grid].split.join(', ')}"
+    elsif !word_valid
+      @msg = "Sorry but <b>#{params[:word].upcase}</b> does not seem to be a valid English word..."
     else
-      @msg = "<b>Congratulations!</b> #{params[:word]} is a valid English word!"
+      @msg = "<b>Congratulations!</b> #{params[:word].upcase} is a valid English word!"
     end
   end
+
+  private
 
   def word_valid?(word, grid)
     word.upcase.each_char do |char|
